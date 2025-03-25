05d8d8eee5496e8e03f1ed51fdf2932d972e83fb
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2025-03-25 18:37:37 +0300

    Add test

---
+++ run.py
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env python3
+import os
+import platform
+import subprocess
+
+subprocess.call(["npm", "install", "http-server", "-g"], stderr=subprocess.STDOUT, shell=True)
+subprocess.call(["http-server", "-p", "9500", "--cors"], stderr=subprocess.STDOUT, shell=True)
