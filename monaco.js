82c43894df5b7d014a508db904e2e02883de4708
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2025-04-11 10:03:13 +0300

    Fix typo

--- monaco.js
+++ monaco.js
@@ -2,7 +2,7 @@
 
     var LOADED_STATUS = false;
     //var LOADING_URL = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.2/min/vs/loader.min.js";
-    var LOADING_URL = "./monaco/min/vs/loader.min.js";
+    var LOADING_URL = "./monaco/min/vs/loader.js";
 
     var REGISTERED_EDITORS = [];
 
